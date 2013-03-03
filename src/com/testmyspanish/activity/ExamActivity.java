82bfa26e4e6d54e208822050f2d936aeb49a7f68
Henrique Rezende <henriquerrezende@gmail.com>
2013-03-03 17:32:05 -0300

    Changing names to Exam instead of Question, with all the logic in ExamActivity. Including ExamDAO. When click NextQuestion button, we call onCreate again.

---
+++ src/com/testmyspanish/activity/ExamActivity.java
@@ -0,0 +1,85 @@
+package com.testmyspanish.activity;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.testmyspanish.R;
+import com.testmyspanish.model.Answer;
+import com.testmyspanish.model.Question;
+import com.testmyspanish.persistence.dao.QuestionDAO;
+
+public class ExamActivity extends Activity {
+
+	private Long correctAnswerId;
+	private Toast answerMessage;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.exam_layout);
+
+	    Question question = QuestionDAO.readRandomQuestion();
+	    correctAnswerId = question.getCorrectAnswerId();
+
+	    TextView questionTextView = (TextView) findViewById(R.id.question);
+	    questionTextView.setText(question.getQuestion());
+
+	    RadioGroup answersOptionsGroup = (RadioGroup) findViewById(R.id.answerOptionsGroup);
+	    for(Answer answerOption : question.getAnswers()) {
+			RadioButton answerButton = new RadioButton(this);
+			answerButton.setId(answerOption.getId());
+			answerButton.setText(answerOption.getAnswer());
+			answerButton.setOnClickListener(answerButtonClickHandler);
+			answersOptionsGroup.addView(answerButton);
+	    }
+
+	    answerMessage = Toast.makeText(ExamActivity.this, "", Toast.LENGTH_SHORT);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	private OnClickListener answerButtonClickHandler = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			RadioButton answerButton = (RadioButton) v;
+			if (Long.valueOf(answerButton.getId()) == correctAnswerId) {
+				answerButton.setTextColor(Color.GREEN);
+				answerMessage.setText("Question answered correctly!");
+				answerMessage.show();
+			} else {
+				answerButton.setTextColor(Color.RED);
+				answerMessage.setText("Question answered wrong!");
+				answerMessage.show();
+			}
+
+		    disableAnswerButtons(answerButton);
+		}
+
+		private void disableAnswerButtons(View radioButtonView) {
+			RadioGroup answersOptionsGroup = (RadioGroup) radioButtonView.getParent();
+		    Integer radioButtonCount = answersOptionsGroup.getChildCount();
+
+		    for (int i=0; i<radioButtonCount; i++){
+				answersOptionsGroup.getChildAt(i).setEnabled(false);
+		    }
+		}
+	};
+
+	public void nextQuestion(View v) {
+		answerMessage.cancel();
+		this.onCreate(null);
+	}
+}
